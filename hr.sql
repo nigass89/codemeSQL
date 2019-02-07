35ff3e978a2d1b8e9d72384bd5140d316347752a
Yellok <s165260@student.pg.gda.pl>
2019-02-07 18:27:48 +0100

    Update and rename hr6.sql to hr.sql

--- hr.sql
+++ hr.sql
@@ -4,7 +4,3 @@ delete from pracownicy where employee_id=(select employee_id from (SELECT employ
 
 --z dwoch managerow z najmniejsza iloscia zrobic zespol i jednego z nich dac pod niego
 select * from (select manager_id,count(*) from employees where manager_id is not null group by manager_id order by count(*)) where rownum<=2;
-
-
-rollback
-
